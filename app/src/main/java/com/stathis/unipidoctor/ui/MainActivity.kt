1b4c62062399035381b085fa75fc64f7c3a78219
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2023-08-13 16:02:41 +0300

    project init

---
+++ app/src/main/java/com/stathis/unipidoctor/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stathis.unipidoctor.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stathis.unipidoctor.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
